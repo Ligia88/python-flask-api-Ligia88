c757771372317a8180e7e72ecad32d6cd796153c
Ligia88 Elizondo <lizcr1988@gmail.com>
2023-08-17 02:45:47 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -3,22 +3,38 @@ app = Flask(__name__)
 
 # Global variable containing the list of todos
 todos = [
-    { "label": "feed the cat", "done": False },
-    { "label": "Make the bed", "done": False }
+    { "label": "My first task", "done": False }
 ]
 
-# Define the route for '/todos' (GET method)
 @app.route('/todos', methods=['GET'])
-def get_todos():
-    # Return the JSON version of the todos list
-    return jsonify(todos)
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
 
 # Define the route for '/todos' (POST method)
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.get_json(force=True)
-    todos.append(request_body)
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    
+    # Add the new todo to the todos list
+    new_todo = {
+        "label": request_body.get("label"),
+        "done": request_body.get("done", False)
+    }
+    todos.append(new_todo)
+    
+    # Return the updated list of todos
     return jsonify(todos)
+
+# Define the route for '/todos/<int:position>' (DELETE method)
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        deleted_todo = todos.pop(position)
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Invalid position"}), 400
 
 # Keep the lines below at the end of your app.py file
 if __name__ == '__main__':
